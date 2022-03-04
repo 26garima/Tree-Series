678dd831bcbef3bf9a73cdbed2214d94a952c7d0
Garima Kushwaha <93420514+26garima@users.noreply.github.com>
2022-03-04 16:08:26 +0530

    Update Search_In_A_BST.java

--- Search in a Binary Search Tree/Search_In_A_BST.java
+++ Search in a Binary Search Tree/Search_In_A_BST.java
@@ -1,3 +1,4 @@
+//  RECURSIVE APPROACH
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
         if(root == null)
@@ -6,6 +7,22 @@ class Solution {
             return searchBST(root.right, val);
         if(root.val > val) 
             return searchBST(root.left, val);
+        return root;
+    }
+}
+
+
+// ITERATIVE APPROACH
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        while(root != null) {
+            if(root.val < val)
+                root = root.right;
+            else if(root.val > val)
+                root = root.left;
+            else
+                break;
+        }
         return root;
     }
 }
