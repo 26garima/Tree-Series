8b063682aae7314385e0e9f541d5af9d71e43a57
Garima Kushwaha <93420514+26garima@users.noreply.github.com>
2022-03-04 12:15:23 +0530

    Create PreOrder_Traversal.java

---
+++ Morris Traversal (Pre-Order)/PreOrder_Traversal.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> preorder = new ArrayList<>();
+        TreeNode curr = root;
+        
+        while(curr != null) {
+            if(curr.left == null) {
+                preorder.add(curr.val);
+                curr = curr.right;
+            } else {
+                TreeNode temp = curr.left;
+                while(temp.right != null && temp.right != curr) {
+                    temp = temp.right;
+                }
+                if(temp.right == null) {
+                    temp.right = curr;
+                    preorder.add(curr.val);
+                    curr = curr.left;
+                } else {
+                    temp.right = null;
+                    curr = curr.right;
+                }
+            }
+        }
+        
+        return preorder;
+    }
+}
