901f35f0122eea7251532463c6270cd28cdba124
Garima Kushwaha <93420514+26garima@users.noreply.github.com>
2022-03-04 13:09:59 +0530

    Create Flatten_Binary_Tree.java

---
+++ Flatten Binary Tree to Linked List (Iterative Approach)/Flatten_Binary_Tree.java
@@ -0,0 +1,19 @@
+class Solution {
+    Stack<TreeNode> st = new Stack<>();
+    public void flatten(TreeNode root) {
+        if(root == null)
+            return;
+        
+        st.push(root);
+        while(!st.isEmpty()) {
+            TreeNode curr = st.pop();
+            if(curr.right != null)
+                st.push(curr.right);
+            if(curr.left != null)
+                st.push(curr.left);
+            curr.left = null;
+            if(!st.isEmpty())
+                curr.right = st.peek();
+        }
+    }
+}
